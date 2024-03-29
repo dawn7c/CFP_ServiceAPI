ae41e03ebf74ddc36276ad03fc6c063fa2303f0d
Ilya <powaliaew.il@yandex.ru>
2024-03-29 17:37:48 +0400

    Добавьте файлы проекта.

---
+++ Domain/Models/Bid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Bid : BaseEntity
+    {
+        public User BidId { get; set; }
+        public TypeOfActivity TypeOfActivity { get; set; }
+
+        [MaxLength(100)]
+        public string Title {  get; set; }
+
+        [MaxLength (300)]
+        public string? ShortDescription {  get; set; }
+
+        [MaxLength (1000)]
+        public string Plan { get; set; }
+
+    }
+}
